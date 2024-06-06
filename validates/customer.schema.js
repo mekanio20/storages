8fca810dfe256eba1c8dad1c653e52dd39f7b2bb
mekanio20 <mrxyok138@gmail.com>
2024-06-06 19:34:53 +0500

    customer edit completed

--- validates/customer.schema.js
+++ validates/customer.schema.js
@@ -6,6 +6,10 @@ const customerSchema = {
         fullname: Joi.string().min(3).max(30).regex(/^[a-zA-ZÄäŇňÖöŞÜüÇçÝý\s]+$/).messages({'string.pattern.base': 'Invalid fullname'}).required(),
         gender: Joi.string().valid('male', 'fmale').required(),
         email: Joi.string().email().required()
+    }),
+    customerEditProfile: Joi.object({
+        fullname: Joi.string().min(3).max(30).regex(/^[a-zA-ZÄäŇňÖöŞÜüÇçÝý\s]+$/).messages({'string.pattern.base': 'Invalid fullname'}).required(),
+        email: Joi.string().email().optional()
     })
 }
 
